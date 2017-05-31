95a63542e912d2c818fef381eb62dabf93370727
sam <sam@ma-work.co.uk>
2017-05-31 17:20:41 +0100

    retreieve as slug

--- src/website/article/views.py
+++ src/website/article/views.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets
 from .models import *
+from django.shortcuts import get_object_or_404
 from .serializers import *
+from rest_framework.response import Response
 
 
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Article.public.all()
     serializer_class = ArticleSerialiers
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, slug=pk)
+        serializer = ArticleSerialiers(article)
+        return Response(serializer.data)
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
