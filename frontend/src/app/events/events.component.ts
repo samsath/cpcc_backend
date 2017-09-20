fbd4c35677b944b49cf5118b5dae714a432cab44
sam <sam@ma-work.co.uk>
2017-09-21 00:13:05 +0100

    tweak the graph data times

--- frontend/src/app/events/events.component.ts
+++ frontend/src/app/events/events.component.ts
@@ -34,11 +34,41 @@ export class EventsComponent implements OnInit {
   daytide:Array<any> = [
     {data: [], label: 'Tide'},
   ];
-  tideoptions: any = {
+  tideoptionslarge: any = {
     responsive:false,
     maintainAspectRatio: false,
     legend:{display:false},
-    scales:{yAxes:[{display:true}],xAxes:[{display:true,type: 'linear',position:'bottom'}]}
+    tooltips :{
+      enable:true,
+      mode: 'single',
+      callbacks:{
+        label: function(tooltipItems, data){
+          return tooltipItems.yLabel + ' meters';
+        },
+        title: function (tooltipItems, data) {
+          let date = new Date(null);
+          date.setSeconds(tooltipItems[0].xLabel);
+          return 'Time: ' + date.toISOString().substr(14, 5);
+        }
+      }
+    },
+    scales:{yAxes:[{display:true, labelString:'Level (m)', ticks: {
+      callback: function (value, index, values) {
+        return value +'m';
+      }
+    }}],
+      xAxes:[{display:true, type: 'linear',position:'bottom', labelString:'Time of day',ticks:{
+        callback: function(value, index, values){
+          let date = new Date(null);
+          date.setSeconds(value);
+          let time = date.toISOString().substr(14, 5);
+          if (time == '25:00'){
+            return '23:59';
+          }else{
+            return time;
+          }
+        }
+      }}]}
   };
   tideColour:Array<any> = [{
     backgroundColor: 'transparent',
@@ -89,6 +119,7 @@ export class EventsComponent implements OnInit {
       legend:{display:false},
       scales:{yAxes:[{display:false}],xAxes:[{display:false,type: 'linear',position:'bottom'}]}
     };
+
     const colour:Array<any> = [{
       backgroundColor: 'transparent',
       borderColor: '#2287b0',
@@ -111,7 +142,7 @@ export class EventsComponent implements OnInit {
 
   openModal(date: Date) {
     this.clickDate = date;
-    this.getDate()
+    this.getDate();
     this.modalActions.emit({action:"modal",params:['open']});
   }
   closeModal() {
