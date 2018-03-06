e72d8c34a5b3804bbbd96ec0585cb984bd932a2c
sam <sam@ma-work.co.uk>
2018-03-06 00:02:17 +0000

    tweak the subscription request

--- frontend/src/app/enquiry/enquiry.component.ts
+++ frontend/src/app/enquiry/enquiry.component.ts
@@ -20,7 +20,7 @@ export class EnquiryComponent implements OnInit {
   mailcomplete = false;
 
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private jsonp: Jsonp) {
 
   }
 
@@ -77,11 +77,12 @@ export class EnquiryComponent implements OnInit {
     headers.append('Access-Control-Allow-Headers', 'Content-Type');
     headers.append('Access-Control-Allow-Methods', 'GET');
     headers.append('Access-Control-Allow-Methods', 'POST');
+    headers.append('Access-Control-Allow-Methods', 'OPTIONS');
     headers.append('Access-Control-Allow-Origin', '*');
-    this.http
-      .post('//chiswickcanoeclub.us10.list-manage.com/subscribe/post?u=608521f94ccfeee308925028f&amp;id=1068db21b2',
-        JSON.stringify({ EMAIL: form.email, FNAME: form.name, LNAME: form.last_name, b_608521f94ccfeee308925028f_1068db21b2: form.b_608521f94ccfeee308925028f_1068db21b2  }),{headers: headers})
-      .subscribe( ret_data => {
+    let url ='//chiswickcanoeclub.us10.list-manage.com/subscribe/post-json?u=608521f94ccfeee308925028f&amp;id=1068db21b2&subscribe=Subscribe&EMAIL='
+      + form.email + '&FNAME=' + form.name + '&LNAME=' + form.lastname + '&b_608521f94ccfeee308925028f_1068db21b2='
+      + form.b_608521f94ccfeee308925028f_1068db21b2 + '&callback=JSONP_CALLBACK';
+    this.jsonp.request(url, {method: 'Get'}).subscribe( ret_data => {
         console.log(ret_data);
         this.mailcomplete = true;
         this.mailForm.reset();
